36c4a7cf54d41ef2dfe6b7b0175dffee26fc9bb8
Your Name <you@example.com>
2020-02-25 10:04:33 +0200

    finish

--- src/api/recipesApi.js
+++ src/api/recipesApi.js
@@ -1,5 +1,6 @@
-const getRecipes = () => fetch('http://localhost:5000/recipes/')
+const getRecipes = () => fetch('https://secure-spire-78632.herokuapp.com/recipes/')
   .then(response => response.json())
   .catch(() => 'Something wrong');
 
 export default getRecipes;
+
