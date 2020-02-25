4fa735fea0c526b57556d6e71248366e798c15ba
Your Name <you@example.com>
2020-02-25 10:08:55 +0200

    Updates

---
+++ precache-manifest.4d2dc3f44257273210b94784bf6cfbc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cbfde318cf980e51571db7ad8f2992",
+    "url": "/Binary_Studio_JS_cookBook/index.html"
+  },
+  {
+    "revision": "e8785a8b534412fc4c4f",
+    "url": "/Binary_Studio_JS_cookBook/static/css/main.8e6d07b1.chunk.css"
+  },
+  {
+    "revision": "d7622deaf6fa764649a1",
+    "url": "/Binary_Studio_JS_cookBook/static/js/2.d35e131a.chunk.js"
+  },
+  {
+    "revision": "e8785a8b534412fc4c4f",
+    "url": "/Binary_Studio_JS_cookBook/static/js/main.5f531668.chunk.js"
+  },
+  {
+    "revision": "dfa91419847a174c08b9",
+    "url": "/Binary_Studio_JS_cookBook/static/js/runtime~main.76d1d77c.js"
+  },
+  {
+    "revision": "4c33f13ff3bdc5d3af45ceb7cd4cccc0",
+    "url": "/Binary_Studio_JS_cookBook/static/media/appStore.4c33f13f.png"
+  },
+  {
+    "revision": "07f9f0fbbfa7feb10592bf87ea2f59c7",
+    "url": "/Binary_Studio_JS_cookBook/static/media/googlePlay.07f9f0fb.png"
+  },
+  {
+    "revision": "d43e42b0df8c2ef81120100a8ef9fc20",
+    "url": "/Binary_Studio_JS_cookBook/static/media/vortrittssignal001-stop.d43e42b0.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Binary_Studio_JS_cookBook/precache-manifest.4d2dc3f44257273210b94784bf6cfbc2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Binary_Studio_JS_cookBook/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
